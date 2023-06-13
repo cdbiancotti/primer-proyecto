e0ca2110a57fa9174e8266b6e5a18b99a0ce358e
Cristian D. Biancotti <cdbiancotti@gmail.com>
2023-06-12 22:33:43 -0300

    Terminamos clase Playground Intermedio parte I

--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,101 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Template, Context, loader
+from inicio.models import Perro
 from django.shortcuts import render
 
-# Create your views here.
+# v1
+# def inicio(request):
+#     return HttpResponse('Hola soy tu inicio')
+
+# v2  
+# def inicio(request):
+#     archivo = open(r'C:\Users\cdbia\Desktop\proytectos\mi_primer_django\templates\inicio.html', 'r')
+#     template = Template(archivo.read())
+#     archivo.close()
+#     segundos = datetime.now().second
+#     diccionario = {
+#         'mensaje': 'Este es el mensaje de inicio...',
+#         'segundos': segundos,
+#         'segundo_par': segundos%2 == 0,
+#         'segundo_redondo': segundos%10 == 0,
+#         'listado_de_numeros': list(range(25))
+#     }
+#     contexto = Context(diccionario)
+#     renderizar_template = template.render(contexto)
+#     return HttpResponse(renderizar_template)
+
+# v3
+# def inicio(request):
+#     # archivo = open(r'C:\Users\cdbia\Desktop\proytectos\mi_primer_django\templates\inicio.html', 'r')
+#     # template = Template(archivo.read())
+#     # archivo.close()
+    
+#     template = loader.get_template('inicio.html')
+    
+#     segundos = datetime.now().second
+#     diccionario = {
+#         'mensaje': 'Este es el mensaje de inicio...',
+#         'segundos': segundos,
+#         'segundo_par': segundos%2 == 0,
+#         'segundo_redondo': segundos%10 == 0,
+#         'listado_de_numeros': list(range(25))
+#     }
+#     # contexto = Context(diccionario)
+#     # renderizar_template = template.render(contexto)
+    
+#     renderizar_template = template.render(diccionario)
+#     return HttpResponse(renderizar_template)
+
+# v4
+def prueba(request):
+    # template = loader.get_template('inicio.html')
+    
+    segundos = datetime.now().second
+    diccionario = {
+        'mensaje': 'Este es el mensaje de inicio...',
+        'segundos': segundos,
+        'segundo_par': segundos%2 == 0,
+        'segundo_redondo': segundos%10 == 0,
+        'listado_de_numeros': list(range(25))
+    }
+    # renderizar_template = template.render(diccionario)
+    # return HttpResponse(renderizar_template)
+    return render(request, 'inicio/prueba.html', diccionario)
+    
+def inicio(request):
+    return render(request, 'inicio/inicio.html')
+
+def segunda_vista(request):
+    return HttpResponse('<h1>Soy la segunda vista!</h1>')
+
+def fecha_actual(request):
+    fecha = datetime.now()
+    return HttpResponse(f'<h1>Fecha actual: {fecha}</h1>')
+
+def saludar(request):
+    return HttpResponse('Bienvenido/a!!!')
+
+def bienvenida(request, nombre, apellido):
+    return HttpResponse(f'Bienvenido/a {nombre.title()} {apellido.title()}!!!')
+
+# v1
+# def crear_perro(request, nombre, edad):
+#     template = loader.get_template('crear_perro.html')
+#     perro = Perro(nombre=nombre, edad=edad)
+#     perro.save()
+#     diccionario = {
+#         'perro': perro,
+#     }
+#     renderizar_template = template.render(diccionario)
+#     return HttpResponse(renderizar_template)
+
+# v2
+def crear_perro(request, nombre, edad):
+    perro = Perro(nombre=nombre, edad=edad)
+    perro.save()
+    diccionario = {
+        'perro': perro,
+    }
+    return render(request, 'inicio/crear_perro.html', diccionario)
+    
--- miprimerdjango/urls.py
+++ miprimerdjango/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-# from miprimerdjango.views import inicio, segunda_vista, fecha_actual
-from miprimerdjango import views
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('inicio.urls')),
     path('admin/', admin.site.urls),
-    path('', views.inicio),
-    path('segunda-vista/', views.segunda_vista),
-    path('fecha-actual/', views.fecha_actual),
-    path('saludar/', views.saludar),
-    path('saludar/<str:nombre>/<str:apellido>/', views.bienvenida),
-    path('crear-perro/<str:nombre>/<int:edad>/', views.crear_perro),
 ]
